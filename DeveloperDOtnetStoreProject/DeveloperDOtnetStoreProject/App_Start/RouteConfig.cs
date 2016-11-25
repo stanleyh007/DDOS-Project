8cd07fec98cb4448e663940efdd2ca69ba397f03
Xinkai Huang <stanley_h007@hotmail.com>
2016-11-25 03:15:45 +0100

    HomeView

--- DeveloperDOtnetStoreProject/DeveloperDOtnetStoreProject/App_Start/RouteConfig.cs
+++ DeveloperDOtnetStoreProject/DeveloperDOtnetStoreProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DeveloperDOtnetStoreProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CategoryItem", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
