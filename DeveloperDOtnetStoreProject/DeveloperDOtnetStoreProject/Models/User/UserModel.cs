def790ce23715215d80fc644de19436b895d23ce
Xinkai Huang <stanley_h007@hotmail.com>
2016-12-01 03:51:16 +0100

    Under Development

--- DeveloperDOtnetStoreProject/DeveloperDOtnetStoreProject/Models/User/UserModel.cs
+++ DeveloperDOtnetStoreProject/DeveloperDOtnetStoreProject/Models/User/UserModel.cs
@@ -9,9 +9,9 @@ namespace DeveloperDOtnetStoreProject.Models.User
     public class UserModel
     {
         public int Id { get; set; }
-        public string ForName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Adddress { get; set; }
+        public string Address { get; set; }
         public string PostalCode { get; set; }
         public string City { get; set; }
         public string Email { get; set; }
